bbb84e13c4a69b80802e29ce7feb94b564385e53
Andreza <andreza_menezes@hotmail.com>
2024-06-03 20:17:14 -0300

    atualizaçao dos retourn

--- src/main/java/Care/Paciente.java
+++ src/main/java/Care/Paciente.java
@@ -20,14 +20,14 @@ public class Paciente {
 
     public String diagnostico() {
         double imc = calcularIMC();
-        if (imc < 16) return "Baixo peso muito grave";
-        else if (imc < 17) return "Baixo peso grave";
-        else if (imc < 18.5) return "Baixo peso";
-        else if (imc < 25) return "Peso normal";
-        else if (imc < 30) return "Sobrepeso";
-        else if (imc < 35) return "Obesidade grau I";
-        else if (imc < 40) return "Obesidade grau II";
-        else return "Obesidade grau III (obesidade mórbida)";
+        if (imc < 16) return "Baixo peso muito grave = IMC abaixo de 16 kg/m²";
+        else if (imc < 17) return "Baixo peso grave = IMC entre 16 e 16,99 kg/m²";
+        else if (imc < 18.5) return "Baixo peso = IMC entre 17 e 18,49 kg/m²";
+        else if (imc < 25) return "Peso normal = IMC entre 18,50 e 24,99 kg/m²";
+        else if (imc < 30) return "Sobrepeso = IMC entre 25 e 29,99 kg/m²";
+        else if (imc < 35) return "Obesidade grau I = IMC entre 30 e 34,99 kg/m²";
+        else if (imc < 40) return "Obesidade grau II = IMC entre 35 e 39,99 kg/m²";
+        else return "Obesidade grau III (obesidade mórbida) = IMC igual ou maior que 40 kg/m²";
     }
 
 
